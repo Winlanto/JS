a376edc9d0d3d84cc1382b4344da8cef97b3780a
Alek-Jaan Tsern <winlanto@tlu.ee>
2022-07-01 02:17:36 +0300

    upd

--- JQuery/onload animation/src/js/loader.js
+++ JQuery/onload animation/src/js/loader.js
@@ -25,13 +25,12 @@ function getNames() {
     animate();
 }
 
-async function animate() {
-    console.log(images.length);
+function animate() {
     $("#loader").append("<div id='imgDiv'></div>");
     for(let i = 0; i < images.length; i++) {
-        console.log(i);
         $("#imgDiv").empty().append("<img src='img/loader/" + images[i] + "' alt='animation image' id='loadingImg'>");
         $("#loadingImg").hide().fadeIn(2000).fadeOut(1000);
+        console.log(i+". cycle done")
     }
     //$("#loader").fadeOut("slow");
 }
